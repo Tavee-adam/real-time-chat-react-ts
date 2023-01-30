d08b5567029f0b6b1021bd3bd4ec0402e6ef8902
Tavee Adam <tavee.am@gmail.com>
2023-01-30 10:52:32 +0700

    add path aliases

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # real-time-chat-react-ts
+
+Adding path aliases as written in article by Alexander Kim 2022
+https://dev.to/avxkim/setup-path-aliases-w-react-vite-ts-poa
--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ type Props = {};
 
 export default function App({}: Props) {
   return (
-    <div className="bg-gray-400">
-      <h1 className="text-5xl underline text-red-800">
+    <div>
+      <h1 className="text-5xl underline text-red-300">
         {import.meta.env.VITE_APP_NAME}
       </h1>
       <img src={imgReact} alt="" />;
